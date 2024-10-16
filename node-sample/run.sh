5a6a96bd487d362d4e51cefa74a4a9563be32474
Mateus Perdigão <mateusperdigao@hotmail.com>
2024-10-16 23:03:23 +0000

    test

--- node-sample/run.sh
+++ node-sample/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-../scripts/run-codeql.sh default --path=$(pwd) --override --language=javascript 
+../scripts/run-codeql.sh default --path=$(pwd) --override --language=javascript
 
 #../scripts/run-codeql.sh security-and-quality --path=$(pwd) --override --language=javascript 
 
