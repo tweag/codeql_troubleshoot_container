32acc9b80b3006cb1442fdc4a1515bd1a0934fc6
Mateus Perdigão <mateusperdigao@hotmail.com>
2024-10-16 20:10:28 -0300

    Revert "PR Analysis without  upload.yml"

---
+++ csharp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your username:");
+            string username = Console.ReadLine();
+
+            // Vulnerable SQL query with string concatenation
+            string query = "SELECT * FROM Users WHERE Username = '" + username + "'";
+
+            using (SqlConnection connection = new SqlConnection("your_connection_string"))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader["Username"]);
+                }
+            }
+
+            Console.WriteLine("Query executed: " + query);
+        }
+    }
+}
