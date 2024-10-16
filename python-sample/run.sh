8190234e19e0e43006bbdddbd8be61e58d5115f7
Mateus Perdigão <mateusperdigao@hotmail.com>
2024-10-16 20:46:22 +0000

    test

--- python-sample/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-../scripts/run-codeql.sh default --path=$(pwd) --override --language=python 
-
-#../scripts/run-codeql.sh security-and-quality --path=$(pwd) --override --language=python 
-
-#../scripts/run-codeql.sh "security-extended" --path=$(pwd) --override --language=python 
